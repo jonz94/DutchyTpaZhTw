0544dc941b15f244dec266048913fdc0298cc5eb
jonz94 <jody16888@gmail.com>
2021-06-29 23:39:07 +0800

    Format coding style

--- src/main/java/dev/jonz94/dutchytpazhtw/SuccessfulTpaEvent.java
+++ src/main/java/dev/jonz94/dutchytpazhtw/SuccessfulTpaEvent.java
@@ -8,49 +8,49 @@ import org.bukkit.event.HandlerList;
 
 public class SuccessfulTpaEvent extends Event implements Cancellable {
 
-    private static final HandlerList HANDLERS_LIST = new HandlerList();
-	
-	private Player player;
-	private Location locationBeforeTpa;
-    private boolean isCancelled;
-	
-	public SuccessfulTpaEvent(Player player, Location locationBeforeTpa) {
-		this.player = player;
-		this.locationBeforeTpa = locationBeforeTpa;
-	}
-	
-    @Override
-    public boolean isCancelled() {
-        return isCancelled;
-    }
+  private static final HandlerList HANDLERS_LIST = new HandlerList();
 
-    @Override
-    public void setCancelled(boolean cancelled) {
-        this.isCancelled = cancelled;
-    }
-	
-	 @Override
-    public HandlerList getHandlers() {
-        return HANDLERS_LIST;
-    }
+  private Player player;
+  private Location locationBeforeTpa;
+  private boolean isCancelled;
 
-    public static HandlerList getHandlerList() {
-        return HANDLERS_LIST;
-    }
-    
-    public Player getPlayer() {
-    	return player;
-    }
-    
-    public void setPlayer(Player player) {
-    	this.player = player;
-    }
-    
-    public Location getLocationBeforeTpa() {
-    	return locationBeforeTpa;
-    }
-    
-    public void setLocationBeforeTpa(Location locationBeforeTpa) {
-    	this.locationBeforeTpa = locationBeforeTpa;
-    }
+  public SuccessfulTpaEvent(Player player, Location locationBeforeTpa) {
+    this.player = player;
+    this.locationBeforeTpa = locationBeforeTpa;
+  }
+
+  @Override
+  public boolean isCancelled() {
+    return isCancelled;
+  }
+
+  @Override
+  public void setCancelled(boolean cancelled) {
+    this.isCancelled = cancelled;
+  }
+
+  @Override
+  public HandlerList getHandlers() {
+    return HANDLERS_LIST;
+  }
+
+  public static HandlerList getHandlerList() {
+    return HANDLERS_LIST;
+  }
+
+  public Player getPlayer() {
+    return player;
+  }
+
+  public void setPlayer(Player player) {
+    this.player = player;
+  }
+
+  public Location getLocationBeforeTpa() {
+    return locationBeforeTpa;
+  }
+
+  public void setLocationBeforeTpa(Location locationBeforeTpa) {
+    this.locationBeforeTpa = locationBeforeTpa;
+  }
 }
--- src/main/java/dev/jonz94/dutchytpazhtw/Tpa.java
+++ src/main/java/dev/jonz94/dutchytpazhtw/Tpa.java
@@ -4,13 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Tpa extends JavaPlugin {
 
-	@Override
-	public void onEnable() {
-		getCommand("tpa").setExecutor(new CommandHandler(this));
-		getCommand("tpaccept").setExecutor(new CommandHandler(this));
-		getCommand("tpdeny").setExecutor(new CommandHandler(this));
-		getCommand("tpyes").setExecutor(new CommandHandler(this));
-		getCommand("tpno").setExecutor(new CommandHandler(this));
-
-	}	
+  @Override
+  public void onEnable() {
+    getCommand("tpa").setExecutor(new CommandHandler(this));
+    getCommand("tpaccept").setExecutor(new CommandHandler(this));
+    getCommand("tpdeny").setExecutor(new CommandHandler(this));
+    getCommand("tpyes").setExecutor(new CommandHandler(this));
+    getCommand("tpno").setExecutor(new CommandHandler(this));
+  }
 }
